563bf4b4111dd09bbfff90509e062ae676edc61a
Emmanuel Aderinto <emmanueladerinto493@gmail.com>
2026-09-11 21:52:33 +0100

    Prepare frontend for deployment

--- src/BrandLoader/BrandLoader.jsx
+++ src/BrandLoader/BrandLoader.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import "./BrandLoader.css";
+import "./Brandloader.css";
 
 const BrandLoader = ({ children }) => {
   const location = useLocation();
